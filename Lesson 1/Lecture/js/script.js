40a2f297363d591d43595d4bcbff2c21257c0e60
Eredin322 <bkmrfrbkmrf326915@mail.ru>
2022-09-29 22:01:08 +0600

    Summarized lesson #1

---
+++ Lesson 1/Lecture/js/script.js
@@ -0,0 +1,11 @@
+document.getElementById('out').innerHTML = 'Hello';
+document.getElementById('out').innerHTML = '2019';
+document.getElementById('out').innerHTML = '<b>2020</b>';
+
+document.querySelector('.header').innerHTML = 5;
+document.querySelector('#one').innerHTML = 77;
+
+let a = document.querySelector('#one');
+let c = document.querySelector('.header');
+a.innerHTML = 9999;
+c.innerHTML = 6666;
