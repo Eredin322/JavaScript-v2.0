2249171ee25274cc4a6d602edbbcc92757f8460e
Eredin322 <bkmrfrbkmrf326915@mail.ru>
2022-10-03 16:21:53 +0600

    Wrote down lecture

--- Lesson 3/Lecture/js/script.js
+++ Lesson 3/Lecture/js/script.js
@@ -1,3 +1,39 @@
-if (True) {
-  console.log(123);
+let a = 6;
+
+// if (a > 9) {
+//   //True
+//   console.log('Yes');
+// } else {
+//   console.log('else');
+// }
+
+const button = document.querySelector('button'),
+    input = document.querySelector('.age');
+
+// () => {} стрелочная функция
+button.onclick = () => {
+    let num = +input.value;
+    if (num >= 16 && num < 60) {
+        console.log('Welcome');
+    } else if (num >= 60) {
+        console.log('Ты точно сюда?');
+    } else {
+        console.log('Ты не пройдешь!!!');
+    }
+
+    switch (num) {
+        case 15:
+            console.log('Еще год потерпи');
+            break;
+        case 16:
+            console.log('Ураааа можно!!!');
+            break;
+        default:
+            console.log('oooook');
+    }
 }
+
+let b = 3;
+
+console.log(b > 3 && b < 7); // && - оператор И
+console.log(b > 3 || b < 7); // || - оператор ИЛИ
