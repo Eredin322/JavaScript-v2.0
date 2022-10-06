cb773d1b4e26c278b0da73aec8cc376829ec5420
Eredin322 <bkmrfrbkmrf326915@mail.ru>
2022-10-06 09:26:04 +0600

    Lecture #6

---
+++ Lesson 6/Lecture/js/script.js
@@ -0,0 +1,15 @@
+let out = document.querySelector('.out');
+
+// for (let i = 0; i < 5; i++){
+//   for (let k = 0; k < 10; k++){
+//     out.innerHTML += k;
+//   }
+//   out.innerHTML += '<br>';
+// }
+
+for (let i = 1; i < 10; i++){
+  for (let k = 1; k < 10; k++){
+    out.innerHTML += `${i}*${k}=${i * k}<br>`;
+  }
+  out.innerHTML += '<hr>';
+}
