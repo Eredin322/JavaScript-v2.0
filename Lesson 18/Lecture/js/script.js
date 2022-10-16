30059b25fdd9ddd3f6d010f68cbdcab061404636
Eredin322 <bkmrfrbkmrf326915@mail.ru>
2022-10-16 09:29:54 +0600

    Lecture #18

---
+++ Lesson 18/Lecture/js/script.js
@@ -0,0 +1,12 @@
+let a = 'hello, hi, mahai';
+console.log(a.split(', '));
+
+let b = [8,9,7];
+console.log(b);
+console.log(b.join('-'));
+
+b.forEach(function (elem, index) {
+    console.log(index);
+    console.log(elem*2);
+    console.log('--');
+})
